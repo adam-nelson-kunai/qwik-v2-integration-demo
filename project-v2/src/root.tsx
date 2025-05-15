caa129cd9ac7ae024a1bf1d08cfbfb354178fbd4
Adam R. Nelson <adam@nels.onl>
2025-05-15 12:29:22 -0400

    use qwik v2

--- project-v2/src/root.tsx
+++ project-v2/src/root.tsx
@@ -1,11 +1,10 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, isDev } from "@qwik.dev/core";
 import {
   QwikCityProvider,
   RouterOutlet,
   ServiceWorkerRegister,
-} from "@builder.io/qwik-city";
+} from "@qwik.dev/router";
 import { RouterHead } from "./components/router-head/router-head";
-import { isDev } from "@builder.io/qwik";
 
 import "./global.css";
 
